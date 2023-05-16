1baa721fb64d7a4db7ba1c3a8f71d1441ec0988a
Xinyang Wu <xwu159@myseneca.ca>
2023-05-16 11:38:19 -0400

    change the comment and citation

--- WS01/File.h
+++ WS01/File.h
@@ -1,3 +1,14 @@
+/***********************************************************************
+// Intro. to Object Oriented Programming
+// Workshop 1 Part1
+// Version 1.0
+// -----------------------------------------------------------
+// Name            Date            Email                  Student ID
+//Xinyang Wu     May 16th,2023   xwu159@myseneca.ca       153821228
+//I have done all the coding by myself and only copied the code that my
+  professor provided to complete my workshops and assignments.//
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
 #ifndef SDDS_FILE_H
 #define SDDS_FILE_H
 #include <iostream>
