4117561cfd9bc743e25865ede2d0d07b8e3e04b7
Xinyang Wu <xwu159@myseneca.ca>
2023-05-16 11:36:51 -0400

    Change the comment and citation

--- WS01/File.cpp
+++ WS01/File.cpp
@@ -1,3 +1,14 @@
+/***********************************************************************
+// Intro. to Object Oriented Programming
+// Workshop 1 Part1
+// Version 1.0
+// -----------------------------------------------------------
+// Name            Date            Email                  Student ID
+//Xinyang Wu     May 16th,2023   xwu159@myseneca.ca       153821228
+//I have done all the coding by myself and only copied the code that my
+  professor provided to complete my workshops and assignments.//
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
 #include "File.h"
 
