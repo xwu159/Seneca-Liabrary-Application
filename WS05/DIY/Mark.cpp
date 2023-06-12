8f8023732562b11d2adfd19e40ec0e204081a406
Xinyang Wu <xwu159@myseneca.ca>
2023-06-12 10:00:18 -0400

    comment the code and change char

--- WS05/DIY/Mark.cpp
+++ WS05/DIY/Mark.cpp
@@ -12,23 +12,29 @@
 #include "Mark.h"
 using namespace std;
 namespace sdds {
+	// Default constructor
 	Mark::Mark() {
 		m_mark = 0;
 		m_isValid = true;
 	}
+
+	// Function to check if a mark is valid (0 to 100 inclusive)
 	bool Mark::valid(int mark) const{
 		return (mark >=0 && mark <= 100);
 	}
 
+	// Constructor with one parameter
 	Mark::Mark(int mark) {
 		m_mark = mark;
-		m_isValid = valid(mark);
+		m_isValid = valid(mark);// Check if the mark is valid
 	}
 
+	// Conversion function to int
 	Mark::operator int() const {
 		return m_isValid ?m_mark:0;
 	}
 
+	// += operator overloaded to add a mark to the current object
 	Mark& Mark::operator+=(int mark){
 		if (m_isValid && valid(m_mark + mark)) {
 			m_mark += mark;
@@ -37,14 +43,16 @@ namespace sdds {
 		return *this;
 	}
 
+	// = operator overloaded to assign a mark to the current object
 	Mark& Mark::operator=(int mark) {
 			m_mark = mark;
 			m_isValid = valid(mark);
 		return *this;
 	}
 
+	// Conversion function to double
 	Mark::operator double() const {
-		if (m_isValid&&m_mark >= 0 && m_mark < 50)
+		if (m_mark >= 0 && m_mark < 50)
 			return 0.0;
 		else if (m_mark >= 50 && m_mark < 60)
 			return 1.0;
@@ -57,8 +65,9 @@ namespace sdds {
 		else return 0.0;
 	}
 
+	// Conversion function to char
 	Mark::operator char() const {
-		if (m_mark >= 0 && m_mark < 50)
+		if (m_mark > 0 && m_mark < 50)
 			return 'F';
 		else if (m_mark >= 50 && m_mark < 60)
 			return 'D';
@@ -73,12 +82,14 @@ namespace sdds {
 		}
 	}
 
+	// += operator overloaded to add another Mark object to the current object
 	Mark& Mark::operator+=(const Mark& other) {
 		if (m_isValid && other.m_isValid&& valid(m_mark + other.m_mark))
 			m_mark += other.m_mark;
 		return *this;
 	}
 
+	// += operator overloaded to add a Mark object to an int
 	int operator+=(int& left, const Mark& right) {
 		return left += int(right);
 	}
